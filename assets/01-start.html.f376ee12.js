942781e98eb8bcf133c2fde2ed4f7618c2bc2f2b
Sentiger <Sentiger@users.noreply.github.com>
2023-03-09 09:42:13 +0000

    Deploying to gh-pages from @ Sentiger/sentiger.github.io@d3e70f25bb7ada3c7eb23b926cb1e28efb14dde3 🚀

---
+++ assets/01-start.html.f376ee12.js
@@ -0,0 +1,13 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as a,c as i,e as n}from"./app.c0d587dd.js";const r={},d=n(`<h2 id="\u8FDB\u7A0B\u6A21\u578B" tabindex="-1"><a class="header-anchor" href="#\u8FDB\u7A0B\u6A21\u578B" aria-hidden="true">#</a> \u8FDB\u7A0B\u6A21\u578B</h2><p>master-worker\u8FDB\u7A0B\u6A21\u578B\uFF0Cmaster\u5C06\u8BF7\u6C42\u53D1\u9001\u5230worker\uFF0Cmaster\u53EF\u4EE5\u5C06\u4FE1\u53F7\u53D1\u9001\u7ED9worker</p><h2 id="worker\u62A2\u5360\u8C03\u5EA6" tabindex="-1"><a class="header-anchor" href="#worker\u62A2\u5360\u8C03\u5EA6" aria-hidden="true">#</a> worker\u62A2\u5360\u8C03\u5EA6</h2><h2 id="epoll" tabindex="-1"><a class="header-anchor" href="#epoll" aria-hidden="true">#</a> epoll</h2><p>\u4F7F\u7528<code>epoll</code>\u8FDB\u7A0B\u6A21\u578B\uFF0C\u4E00\u4E2Aworker\u53EF\u4EE5\u63A5\u6536<code>worker_connections</code>\u4E2A\u8BF7\u6C42</p><h2 id="\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#\u914D\u7F6E" aria-hidden="true">#</a> \u914D\u7F6E</h2><p>nginx\u914D\u7F6E\u7ED3\u6784\u6709:</p><ul><li>main\uFF1A\u5168\u5C40\u914D\u7F6E</li><li>event\uFF1A\u914D\u7F6E\u5DE5\u4F5C\u6A21\u5F0F\u4EE5\u53CA\u8FDE\u63A5\u6570</li><li>http\uFF1Ahttp\u6A21\u5757\u914D\u7F6E</li><li>stream\uFF1A\u56DB\u5C42\u8F6C\u53D1\u914D\u7F6E</li></ul><h3 id="main" tabindex="-1"><a class="header-anchor" href="#main" aria-hidden="true">#</a> main</h3><h3 id="event" tabindex="-1"><a class="header-anchor" href="#event" aria-hidden="true">#</a> event</h3><h2 id="location" tabindex="-1"><a class="header-anchor" href="#location" aria-hidden="true">#</a> location</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>location / { # \u9ED8\u8BA4\u5339\u914D\uFF0C\u80FD\u5339\u914D\u5230/*
+
+}
+
+location = / { # \u7CBE\u51C6\u5339\u914D/
+}
+
+location ~* \u6B63\u5219 { # \u6B63\u5219\u5339\u914D\uFF0C*\u8868\u793A\u4E0D\u7F3A\u5206\u5927\u5C0F\u5199
+}
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,12),l=[d];function s(t,c){return a(),i("div",null,l)}var v=e(r,[["render",s],["__file","01-start.html.vue"]]);export{v as default};
---
+++ assets/101-常见查询.html.fda1602f.js
@@ -0,0 +1,36 @@
+import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";import{o as s,c as a,e}from"./app.c0d587dd.js";const i={},l=e(`<p>\u5E38\u89C1\u7684\u67E5\u8BE2sql</p><h2 id="\u7528\u6237\u6210\u7EE9\u6392\u540D" tabindex="-1"><a class="header-anchor" href="#\u7528\u6237\u6210\u7EE9\u6392\u540D" aria-hidden="true">#</a> \u7528\u6237\u6210\u7EE9\u6392\u540D</h2><div class="language-sql ext-sql line-numbers-mode"><pre class="language-sql"><code><span class="token comment">-- set @score=3 \u53EF\u4EE5\u8BBE\u7F6E\u53D8\u91CF</span>
+<span class="token comment">-- select @score:=3 \u4E5F\u53EF\u4EE5\u8BBE\u7F6E\u53D8\u91CF</span>
+ <span class="token keyword">SELECT</span>
+     <span class="token identifier"><span class="token punctuation">\`</span>name<span class="token punctuation">\`</span></span><span class="token punctuation">,</span>
+     score<span class="token punctuation">,</span>
+     <span class="token keyword">CASE</span>
+
+         <span class="token keyword">WHEN</span> <span class="token variable">@temp_score</span> <span class="token operator">=</span> score <span class="token keyword">THEN</span>
+             <span class="token variable">@rank</span>
+         <span class="token keyword">WHEN</span> <span class="token variable">@temp_score</span> :<span class="token operator">=</span> score <span class="token keyword">THEN</span>
+             <span class="token variable">@rank</span> :<span class="token operator">=</span> <span class="token variable">@rank</span> <span class="token operator">+</span> <span class="token number">1</span>
+         <span class="token keyword">END</span> <span class="token keyword">AS</span> rank
+ <span class="token keyword">FROM</span>
+     s_score<span class="token punctuation">,</span>
+     <span class="token punctuation">(</span>
+         <span class="token keyword">SELECT</span>
+             <span class="token variable">@rank</span> :<span class="token operator">=</span> <span class="token number">0</span><span class="token punctuation">,</span>
+		<span class="token variable">@temp_score</span> :<span class="token operator">=</span> <span class="token boolean">NULL</span>
+     <span class="token punctuation">)</span> q
+ <span class="token keyword">ORDER</span> <span class="token keyword">BY</span>
+     score <span class="token keyword">DESC</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u67E5\u8BE2\u7528\u6237\u5217\u8868-\u7EDF\u8BA1\u67E5\u8BE2\u51FA\u6765\u7684ip\u51FA\u73B0\u7684\u6B21\u6570" tabindex="-1"><a class="header-anchor" href="#\u67E5\u8BE2\u7528\u6237\u5217\u8868-\u7EDF\u8BA1\u67E5\u8BE2\u51FA\u6765\u7684ip\u51FA\u73B0\u7684\u6B21\u6570" aria-hidden="true">#</a> \u67E5\u8BE2\u7528\u6237\u5217\u8868\uFF0C\u7EDF\u8BA1\u67E5\u8BE2\u51FA\u6765\u7684IP\u51FA\u73B0\u7684\u6B21\u6570</h2><p>\u4E00\u822C\u90FD\u662F\u5BF9group\u8FDB\u884Cgroup by\uFF0C\u4F46\u662F\u9700\u6C42\u662F\u60F3\u8981\u67E5\u8BE2\u6240\u6709\u5217\u8868\uFF0C\u7136\u540E\u5B57\u6BB5\u51FA\u73B0\u6B21\u6570</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code> SELECT
+	id,
+	last_login_ip,
+	<span class="token punctuation">(</span>
+	SELECT
+		count<span class="token punctuation">(</span> * <span class="token punctuation">)</span> 
+	FROM
+		member AS mi 
+	WHERE
+		mi.last_login_ip <span class="token operator">=</span> member.last_login_ip 
+		AND mi.last_login_ip <span class="token operator">!=</span> <span class="token string">&#39;&#39;</span> 
+	<span class="token punctuation">)</span> as n
+FROM
+	member<span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),t=[l];function p(c,o){return s(),a("div",null,t)}var v=n(i,[["render",p],["__file","101-\u5E38\u89C1\u67E5\u8BE2.html.vue"]]);export{v as default};
